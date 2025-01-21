b9acfc438a11de3da7a5c87d9d644bfd79537f69
Christian Gram Kalhauge <chrg@dtu.dk>
2025-01-21 15:46:30 +0100

    Upgrade base

--- flake.nix
+++ flake.nix
@@ -1,34 +1,31 @@
 {
   description = "A library for reading Java class-files";
 
-  inputs =
-    {
-      nixpkgs.url = "github:NixOS/nixpkgs/23.05";
-      flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-    flake-utils.lib.eachSystem [ "x86_64-darwin" ] (system:
-    let
-      pkgs = (import nixpkgs { inherit system; });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachSystem ["x86_64-darwin"] (system: let
+      pkgs = import nixpkgs {inherit system;};
       haskellPackages = pkgs.haskellPackages;
       project = returnShellEnv:
         haskellPackages.developPackage {
           inherit returnShellEnv;
           root = self;
           name = "jvm-binary";
-          source-overrides = { };
-          overrides = hsuper: hself: { };
+          source-overrides = {};
+          overrides = hsuper: hself: {};
           modifier = drv:
             pkgs.haskell.lib.addBuildTools drv
-              (with haskellPackages; [ cabal-install ghcid haskell-language-server hpack fourmolu ]);
+            (with haskellPackages; [cabal-install ghcid haskell-language-server hpack fourmolu]);
         };
-    in
-    {
+    in {
       defaultPackage = project false;
       devShell = project true;
     });
